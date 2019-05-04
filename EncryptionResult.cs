c2ee02c526d102fbee1350a601f8f1379af06987
Ragnar Stølsmark <rstolsmark@live.no>
2019-05-04 16:21:22 +0200

    Changed to password and key derivation

--- EncryptionResult.cs
+++
@@ -1,6 +0,0 @@
-namespace Rstolsmark.EncryptionLib{
-    public class EncryptionResult{
-        public string Key { get; set; }
-        public string EncryptedData { get; set; }
-    }
-}
--- StringEncryptor.cs
+++ StringEncryptor.cs
@@ -1,16 +1,33 @@
 ﻿using System;
 using System.IO;
 using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 
 namespace Rstolsmark.EncryptionLib
 {
     public static class StringEncryptor
     {
-        public static EncryptionResult Encrypt(string data, string key = null){
-            using(Aes myAes = Aes.Create()){                
-                if(!string.IsNullOrEmpty(key)){
-                    myAes.Key = Convert.FromBase64String(key);
-                }
+        private static byte[] DeriveKey(string password, byte[] salt){
+            return KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: 10000,
+                numBytesRequested: 32);
+        }
+        public static string Encrypt(string data, string password)
+        {
+            // generate a 128-bit salt using a secure PRNG
+            byte[] salt = new byte[16];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+            // derive a 256-bit subkey (use HMACSHA1 with 10,000 iterations)
+            byte[] key = DeriveKey(password, salt);
+            using (Aes myAes = Aes.Create())
+            {
+                myAes.Key = key;
                 byte[] encryptedData;
                 // Create an encryptor to perform the stream transform.
                 ICryptoTransform encryptor = myAes.CreateEncryptor();
@@ -25,30 +42,32 @@ namespace Rstolsmark.EncryptionLib
                             swEncrypt.Write(data);
                         }
                     }
-                    encryptedData =  msEncrypt.ToArray();
+                    encryptedData = msEncrypt.ToArray();
                 }
-                byte[] iVAndData = new byte[16 + encryptedData.Length];
-                Buffer.BlockCopy(myAes.IV, 0, iVAndData, 0, 16);
-                Buffer.BlockCopy(encryptedData, 0, iVAndData, 16, encryptedData.Length);
-                return new EncryptionResult{
-                    Key = Convert.ToBase64String(myAes.Key),
-                    EncryptedData = Convert.ToBase64String(iVAndData)
-                };
+                byte[] saltIVAndData = new byte[32 + encryptedData.Length];
+                Buffer.BlockCopy(salt, 0, saltIVAndData, 0, 16);
+                Buffer.BlockCopy(myAes.IV, 0, saltIVAndData, 16, 16);
+                Buffer.BlockCopy(encryptedData, 0, saltIVAndData, 32, encryptedData.Length);
+                return Convert.ToBase64String(saltIVAndData);
             }
         }
-        
-        public static string Decrypt(string encryptedData, string key){
-             using(Aes myAes = Aes.Create()){
-                 myAes.Key = Convert.FromBase64String(key);
-                 byte[] iVAndData = Convert.FromBase64String(encryptedData);
-                 byte[] iv = new byte[16];
-                 byte[] data = new byte[iVAndData.Length - 16];
-                 Buffer.BlockCopy(iVAndData, 0, iv, 0, 16);
-                 Buffer.BlockCopy(iVAndData, 16, data, 0, iVAndData.Length - 16);
-                 myAes.IV = iv;
-                 // Create a decryptor to perform the stream transform.
-                ICryptoTransform decryptor = myAes.CreateDecryptor();
 
+        public static string Decrypt(string encryptedData, string password)
+        {
+            byte[] saltIVAndData = Convert.FromBase64String(encryptedData);
+            byte[] salt = new byte[16];
+            byte[] iv = new byte[16];
+            byte[] data = new byte[saltIVAndData.Length - 32];
+            Buffer.BlockCopy(saltIVAndData, 0, salt, 0, 16);
+            Buffer.BlockCopy(saltIVAndData, 16, iv, 0, 16);
+            Buffer.BlockCopy(saltIVAndData, 32, data, 0, saltIVAndData.Length - 32);
+            byte[] key = DeriveKey(password, salt);
+            using (Aes myAes = Aes.Create())
+            {
+                myAes.Key = key;
+                myAes.IV = iv;
+                // Create a decryptor to perform the stream transform.
+                ICryptoTransform decryptor = myAes.CreateDecryptor();
                 // Create the streams used for decryption.
                 using (MemoryStream msDecrypt = new MemoryStream(data))
                 {
@@ -56,14 +75,13 @@ namespace Rstolsmark.EncryptionLib
                     {
                         using (StreamReader srDecrypt = new StreamReader(csDecrypt))
                         {
-
                             // Read the decrypted bytes from the decrypting stream
                             // and return them in a string.
                             return srDecrypt.ReadToEnd();
                         }
                     }
                 }
-             }
-         }
+            }
+        }
     }
 }
